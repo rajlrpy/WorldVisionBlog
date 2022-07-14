623f944774831a243d0e18a78ba64440a2297b82
rajlrpy@gmail.com <rajlrpy@gmail.com>
2022-07-14 14:50:45 +0530

    latest post added

--- app/Http/Controllers/frontend/FrontendController.php
+++ app/Http/Controllers/frontend/FrontendController.php
@@ -12,10 +12,8 @@ class FrontendController extends Controller
     public function viewByCategory(string $category_slug){
 
         $category = Category::where('slug',$category_slug)->where('status','1')->first();
-
         if($category){
             $posts = Post::withCount('comments')->where('category_id',$category->id)->where('status','1')->paginate(10);
-
         }
         else{
             return redirect(url('/'));
@@ -25,12 +23,12 @@ class FrontendController extends Controller
 
     public function viewByPost(string $category_slug, string $post_slug){
         $category = Category::where('slug',$category_slug)->where('status','1')->first();
-
         if($category){
             $post = Post::withCount('comments')->where('category_id',$category->id)->where('slug',$post_slug)->where('status','1')->first();
+            $latestPosts = Post::with('category')->where('status','1')->latest()->take(3)->get();
             if($post){
                 $comments = Comment::with('user')->where('post_id',$post->id)->latest()->paginate(3);
-                return view('frontend.post.index', compact('post','comments'));
+                return view('frontend.post.index', compact('post','comments','latestPosts'));
             }
             else{
                 return redirect(url('/'));
